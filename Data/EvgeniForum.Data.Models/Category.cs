218fec2a96426a2a108b5dc8635f3db8475ed9c3
EvgeniTrifonov <trifonov.e.v@gmail.com>
2020-12-02 21:10:22 +0200

    Add initial Models

---
+++ Data/EvgeniForum.Data.Models/Category.cs
@@ -0,0 +1,24 @@
+﻿namespace EvgeniForum.Data.Models
+{
+    using System.Collections.Generic;
+
+    using EvgeniForum.Data.Common.Models;
+
+    public class Category : BaseDeletableModel<int>
+    {
+        public Category()
+        {
+            this.Posts = new HashSet<Post>();
+        }
+
+        public string Name { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string ImgUrl { get; set; }
+
+        public virtual ICollection<Post> Posts { get; set; }
+    }
+}
--- Data/EvgeniForum.Data/Migrations/20201202190706_AddInitialModels.Designer.cs
+++ Data/EvgeniForum.Data/Migrations/20201202190706_AddInitialModels.Designer.cs
@@ -4,14 +4,16 @@ using EvgeniForum.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EvgeniForum.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201202190706_AddInitialModels")]
+    partial class AddInitialModels
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
