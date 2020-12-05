4cb087f3ac0d483482655dbb50fa105b0c379eca
EvgeniTrifonov <trifonov.e.v@gmail.com>
2020-12-05 17:20:40 +0200

    Add FontAwesome icons and post content

--- Web/EvgeniForum.Web.ViewModels/Categories/PostInCategoryViewModel.cs
+++ Web/EvgeniForum.Web.ViewModels/Categories/PostInCategoryViewModel.cs
@@ -2,10 +2,20 @@
 {
     using EvgeniForum.Data.Models;
     using EvgeniForum.Services.Mapping;
+    using System;
 
     public class PostInCategoryViewModel : IMapFrom<Post>
     {
+        public DateTime CreatedOn { get; set; }
+
         public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public string ShortContent =>
+            this.Content?.Length > 300
+            ? this.Content?.Substring(0, 300) + "..."
+            : this.Content;
 
         public string UserUserName { get; set; }
 
