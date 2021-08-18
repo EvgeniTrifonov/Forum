126216d078cff990d8141ac452b66eb115ffbfb4
EvgeniTrifonov <trifonov.e.v@gmail.com>
2021-08-18 23:16:23 +0300

    Add admin

--- Data/Forum.Data/Seeding/CategoriesSeeder.cs
+++ Data/Forum.Data/Seeding/CategoriesSeeder.cs
@@ -19,7 +19,7 @@
             var categories = new List<(string Name, string ImgUrl)>
             {
                 ("Sport", "https://media.npr.org/assets/img/2020/06/10/gettyimages-200199027-001-b5fb3d8d8469ab744d9e97706fa67bc5c0e4fa40.jpg"),
-                ("New", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhpiNON39UBXjwmFR3gLH78i-oQe9DWpDkSw&usqp=CAU"),
+                ("News", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhpiNON39UBXjwmFR3gLH78i-oQe9DWpDkSw&usqp=CAU"),
                 ("Music", "https://www.online-tech-tips.com/wp-content/uploads/2019/05/music.png"),
                 ("Universe", "https://www.airlive.net/wp-content/uploads/2020/06/ad15bfd7b0_50164279_rotation-galaxie-spirale-univers.jpg"),
                 ("AI", "https://images.idgesg.net/images/article/2019/11/ai_artificial_intelligence_ml_machine_learning_vector_by_kohb_gettyimages_1146634284-100817775-large.jpg"),
--- Data/Forum.Data/Seeding/RolesSeeder.cs
+++ Data/Forum.Data/Seeding/RolesSeeder.cs
@@ -15,8 +15,23 @@
         public async Task SeedAsync(ApplicationDbContext dbContext, IServiceProvider serviceProvider)
         {
             var roleManager = serviceProvider.GetRequiredService<RoleManager<ApplicationRole>>();
+            var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
 
             await SeedRoleAsync(roleManager, GlobalConstants.AdministratorRoleName);
+
+            var email = "admin@forum.com";
+            var user = await userManager.FindByEmailAsync(email);
+            if (user == null)
+            {
+                var admin = new ApplicationUser
+                {
+                    Email = email,
+                    UserName = email,
+                };
+
+                await userManager.CreateAsync(admin, "admin12");
+                await userManager.AddToRoleAsync(admin, GlobalConstants.AdministratorRoleName);
+            }
         }
 
         private static async Task SeedRoleAsync(RoleManager<ApplicationRole> roleManager, string roleName)
