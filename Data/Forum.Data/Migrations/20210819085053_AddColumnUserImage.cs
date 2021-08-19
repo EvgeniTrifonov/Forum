b442bc009dcb754d7934b0dca5e01ac70aab1dac
EvgeniTrifonov <trifonov.e.v@gmail.com>
2021-08-19 11:55:35 +0300

    Add column UserImage in ApplicationUsers

---
+++ Data/Forum.Data/Migrations/20210819085053_AddColumnUserImage.cs
@@ -0,0 +1,23 @@
+﻿namespace Forum.Data.Migrations
+{
+    using Microsoft.EntityFrameworkCore.Migrations;
+
+    public partial class AddColumnUserImage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "UserImage",
+                table: "AspNetUsers",
+                type: "varbinary(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserImage",
+                table: "AspNetUsers");
+        }
+    }
+}
