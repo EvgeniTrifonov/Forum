f0e9142a8b47f0ddd2a095da27f69394918e18ea
EvgeniTrifonov <trifonov.e.v@gmail.com>
2021-08-21 18:04:35 +0300

    Add admin on Votes

---
+++ Web/Forum.Web/Controllers/VotesEditController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Forum.Data;
+using Forum.Data.Models;
+
+namespace Forum.Web.Controllers
+{
+    public class VotesEditController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public VotesEditController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Votes1
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Votes.Include(v => v.Post).Include(v => v.User);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Votes1/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var vote = await _context.Votes
+                .Include(v => v.Post)
+                .Include(v => v.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (vote == null)
+            {
+                return NotFound();
+            }
+
+            return View(vote);
+        }
+
+        // GET: Votes1/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var vote = await _context.Votes.FindAsync(id);
+            if (vote == null)
+            {
+                return NotFound();
+            }
+            ViewData["PostId"] = new SelectList(_context.Posts, "Id", "UserId", vote.PostId);
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", vote.UserId);
+            return View(vote);
+        }
+
+        // POST: Votes1/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("PostId,UserId,Type,Id,CreatedOn,ModifiedOn")] Vote vote)
+        {
+            if (id != vote.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(vote);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!VoteExists(vote.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["PostId"] = new SelectList(_context.Posts, "Id", "UserId", vote.PostId);
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", vote.UserId);
+            return View(vote);
+        }
+
+        // GET: Votes1/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var vote = await _context.Votes
+                .Include(v => v.Post)
+                .Include(v => v.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (vote == null)
+            {
+                return NotFound();
+            }
+
+            return View(vote);
+        }
+
+        // POST: Votes1/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var vote = await _context.Votes.FindAsync(id);
+            _context.Votes.Remove(vote);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool VoteExists(int id)
+        {
+            return _context.Votes.Any(e => e.Id == id);
+        }
+    }
+}
